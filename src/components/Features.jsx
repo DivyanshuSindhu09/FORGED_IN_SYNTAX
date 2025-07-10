99c07bb70046e04055d1cd8e15fc916cb391fea8
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-07-10 21:13:13 +0530

    framerworks

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 import Card from "../components/Card";
 import RegisterModal from "../components/RegisterModal";
-import { Frameworks } from "../components/FrameWorks";
+import { Frameworks } from "./FrameWorks";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
